6adad320a7ff17d83fb737c2552f327b4554db36
Thanarat Teptan <thanaratteptan@gmail.com>
2026-02-22 23:24:52 +0700

    Add all HackerRank Homework 2

---
+++ project/tests/test_alternating_characters.py
@@ -0,0 +1,27 @@
+import unittest
+from coe_number.alternating_characters import alternatingCharacters
+
+class AlternatingCharactersTest(unittest.TestCase):
+    def test_give_AAAA_should_be_3(self):
+        # Arrange
+        s = "AAAA"
+        # Act
+        result = alternatingCharacters(s)
+        # Assert
+        self.assertEqual(result, 3)
+
+    def test_give_BBBBB_should_be_4(self):
+        self.assertEqual(alternatingCharacters("BBBBB"), 4)
+
+    def test_give_ABABABAB_should_be_0(self):
+        self.assertEqual(alternatingCharacters("ABABABAB"), 0)
+
+    def test_give_AAABBB_should_be_4(self):
+        self.assertEqual(alternatingCharacters("AAABBB"), 4)
+
+    def test_give_empty_string_should_be_0(self):
+        self.assertEqual(alternatingCharacters(""), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+    
